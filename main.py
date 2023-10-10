39766470b6bedd998411bcf50fbefd48ea23cc12
GURUAKASHSM <109951850+GURUAKASHSM@users.noreply.github.com>
2023-10-10 12:28:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
-import pythoncom
-pythoncom.CoInitialize()
 import os
-import pythoncom
 import pytesseract as tess
 from PIL import Image
 from googletrans import Translator
@@ -25,12 +22,41 @@ def index():
 def process():
     # Empty the "uploads" folder before processing
     folder_path = 'uploads'
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            os.remove(file_path)
+    print('image'in request.files)
+    print('user_text' in request.form)
+    print(request.form['user_text'].strip() != '')
 
-    if 'image' in request.files:
+    if 'user_text' in request.form and request.form['user_text'].strip() != '':
+        # Handle text input
+        user_text = request.form['user_text']
+        print(user_text)
+        selected_language = request.form.get('language')
+
+        # Translate the user-provided text to the selected language
+        translator = Translator()
+        translated_text = translator.translate(user_text, src='en', dest=selected_language).text
+
+        # Initialize the text-to-speech engine for the selected language
+        if selected_language == 'ta':
+            tts = gTTS(text=translated_text, lang='ta')
+        elif selected_language == 'hi':
+            tts = gTTS(text=translated_text, lang='hi')
+        else:
+            tts = gTTS(text=translated_text, lang='en')
+
+        # Save the audio file
+        audio_path = 'uploads/audio_speech.mp3'
+        tts.save(audio_path)
+
+        # Wait for the audio file to be saved
+        while not os.path.exists(audio_path):
+            pass
+
+        return render_template('result.html', text=translated_text, selected_language=selected_language, audio_path=audio_path)
+
+
+    elif 'image' in request.files:
+        # Handle image input
         import pythoncom
         pythoncom.CoInitialize()
         # Get the uploaded image file
@@ -71,6 +97,8 @@ def process():
         os.remove(image_path)
 
         return render_template('result.html', text=translated_text, selected_language=selected_language, audio_path=audio_path)
+
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
