77362f7e24f9ab5646de83e0f1bee4af4b5bf6ac
GURUAKASHSM <109951850+GURUAKASHSM@users.noreply.github.com>
2023-10-25 13:09:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -105,4 +105,4 @@ def uploaded_file(filename):
     return send_from_directory('uploads', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
